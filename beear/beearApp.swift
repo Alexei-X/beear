a9a992512bb641735b8a47d847ac6ac60a093adf
alexbataille <alex.bataille@ulb.be>
2025-03-29 15:51:56 +0100

    Initial Commit

---
+++ beear/beearApp.swift
@@ -0,0 +1,17 @@
+//
+//  beearApp.swift
+//  beear
+//
+//  Created by Alex Bataille on 29/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct beearApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ beearTests/beearTests.swift
@@ -0,0 +1,17 @@
+//
+//  beearTests.swift
+//  beearTests
+//
+//  Created by Alex Bataille on 29/03/2025.
+//
+
+import Testing
+@testable import beear
+
+struct beearTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
