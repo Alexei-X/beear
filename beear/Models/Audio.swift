6c7a8e39158e8624139c0619ca12ad16b01ba6bf
alexbataille <alex.bataille@ulb.be>
2025-03-30 11:15:39 +0200

    created Audio class and changed color of Title in ContentView

---
+++ beear/Models/Audio.swift
@@ -0,0 +1,15 @@
+//
+//  Audio.swift
+//  beear
+//
+//  Created by Alex Bataille on 30/03/2025.
+//
+
+import Foundation
+
+class Audio {
+    var path: String
+    init(path: String) {
+        self.path = path
+    }
+}
